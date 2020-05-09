d740b93ecc75bd29cc24700bf55a7c3270ed8ed7
Hiroaki Yutani <yutani.ini@gmail.com>
2020-05-09 22:13:52 +0900

    Use variable number of corners

--- 04_recur_square.rs
+++ 04_recur_square.rs
@@ -1,49 +1,66 @@
-use nannou::geom::quad::Quad;
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).simple_window(view).run();
+    nannou::app(model).run();
 }
 
-struct Model {}
+struct Model {
+    corners: i32,
+}
 
 fn model(app: &App) -> Model {
-    Model {}
+    app.new_window()
+        .size(700, 700)
+        .mouse_pressed(mouse_pressed)
+        .view(view)
+        .build()
+        .unwrap();
+    Model { corners: 3 }
 }
 
-fn get_gapped_point(q: &Quad<Point2>, gap: f32, i: usize) -> Point2 {
-    let i_next = (i + 1) % 4;
-    let v = q[i_next] - q[i];
-    q[i] + v * gap
+fn mouse_pressed(app: &App, model: &mut Model, btn: MouseButton) {
+    match btn {
+        MouseButton::Left => model.corners += 1,
+        MouseButton::Right => model.corners -= 1,
+        _ => {}
+    }
 }
 
-fn get_gapped_quad(q: &Quad<Point2>, gap: f32) -> Quad<Point2> {
-    let q = Quad([
-        get_gapped_point(q, gap, 0),
-        get_gapped_point(q, gap, 1),
-        get_gapped_point(q, gap, 2),
-        get_gapped_point(q, gap, 3),
-    ]);
-    q
+fn get_gapped_quad(p: &Vec<Point2>, gap: f32) -> Vec<Point2> {
+    (0..p.len())
+        .map(|i| {
+            let i_next = (i + 1) % p.len();
+            let v = p[i_next] - p[i];
+            p[i] + v * gap
+        })
+        .collect()
 }
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     draw.background().color(WHITE);
 
-    let r = Rect::from_w_h(600.0, 600.0);
-    let mut q = r.corners();
+    let radius = 300.0;
+    let mut points: Vec<Point2> = (0..model.corners)
+        .map(|i| {
+            let radian = 2.0 * PI * i as f32 / model.corners as f32;
+            let x = radian.sin() * radius;
+            let y = radian.cos() * radius;
+            pt2(x, y)
+        })
+        .collect();
 
     loop {
-        draw.quad()
+        draw.polygon()
             .stroke_weight(2.0)
             .stroke_color(BLACK)
-            .points(q[0], q[1], q[2], q[3]);
+            .points(points.iter().copied());
 
         let gap = 0.50 + 0.499 * (app.elapsed_frames() as f32 / 200.0).cos();
-        q = get_gapped_quad(&q, gap);
-        if (q[0] - q[1]).magnitude() < 10.0 {
+        points = get_gapped_quad(&points, gap);
+
+        if (points[0] - points[1]).magnitude() < 3.0 {
             break;
         }
     }
